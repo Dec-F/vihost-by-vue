b0bddc0bda5d32be3071cd2d9c045c3a3456143e
strongfanfan <463428221@qq.com>
2017-05-20 10:11:14 +0800

    重构路由

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,7 @@ import MenuManagement from 'pages/MenuManagement';
 import Reply from 'pages/Reply.vue';
 import QR from 'pages/QR';
 import MsgManagement from 'pages/MsgManagement';
-import EditID from 'pages/EditID'; 
+import EditID from 'pages/EditID';
 import SettledList from 'pages/SettledList';
 import AddSettled from 'pages/AddSettled';
 import articleList from 'pages/articleList';
@@ -26,28 +26,36 @@ export default new Router({
     mode: 'history',
     base: __dirname,
     routes: [
- 
-        { path: '/',name:'登录', component: Login },
-        { path: '/index',name:'首页', component: Index,
+
+        { path: '/', name: '登录', component: Login },
+        {
+            path: '/index',
+            name: '首页',
+            component: Index,
+            children: [
+                { path: '', name: '', component: MainContent }
+            ]
+        },
+        { path: '/depart', name: '部门首页', component: DepartmentIndex },
+        { path: '/serviceWindow', name: '服务窗' ,component:Index, redirect:'/serviceWindow/menu',
         children:[
-        { path: '', name:'',component: MainContent },
-        { path: '/depart',name:'部门首页', component: DepartmentIndex },
-        {path:'/serviceWindow',name:'服务窗'},
-        { path: '/serviceWindow/menu',name:'菜单管理', component: MenuManagement },
-        { path: '/serviceWindow/reply',name:'回复管理', component: Reply },
-        { path: '/QR',name:'二维码', component: QR},
-        { path: '/MsgManagement',name:'', component: MsgManagement },
-        { path: '/EditId',name:'账号资料', component: EditID },
-        { path: '/SettledList',name:'入驻部门', component: SettledList },
-        { path: '/AddSettled',name:'添加入驻', component: AddSettled },
-        { path: '/articleList',name:'', component: articleList },
-        { path: '/articleColumn',name:'', component: articleColumn },
-        { path: '/BulletinList',name:'', component: BulletinList },
-        { path: '/Announcement',name:'', component: Announcement },
-        { path: '/TagsManagement',name:'', component: TagsManagement },
-        { path: '/DepartmentAccount',name:'', component: DepartmentAccount },
-        { path: '/AddArticle',name:'', component: AddArticle },
+        { path: 'menu', name: '菜单管理', component: MenuManagement },
+        { path: 'reply', name: '回复管理', component: Reply }            
         ]
-     }
+    },
+        { path: '/QR', name: '二维码', component: QR },
+        { path: '/MsgManagement', name: '', component: MsgManagement },
+        { path: '/EditId', name: '账号资料', component: EditID },
+        { path: '/SettledList', name: '入驻部门', component: SettledList },
+        { path: '/AddSettled', name: '添加入驻', component: AddSettled },
+        { path: '/articleList', name: '', component: articleList },
+        { path: '/articleColumn', name: '', component: articleColumn },
+        { path: '/BulletinList', name: '', component: BulletinList },
+        { path: '/Announcement', name: '', component: Announcement },
+        { path: '/TagsManagement', name: '', component: TagsManagement },
+        { path: '/DepartmentAccount', name: '', component: DepartmentAccount },
+        { path: '/AddArticle', name: '', component: AddArticle },
+
+
     ]
 })
