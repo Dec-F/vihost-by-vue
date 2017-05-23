2289b907c462c3c5aaee21538a2caef82070263d
strongfanfan <463428221@qq.com>
2017-05-23 16:46:43 +0800

    Echarts组件

---
+++ src/components/Echarts.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="echarts" ref="echarts" v-bind:style="{ width:width+'px',height:height+'px'}">
+  </div>
+</template>
+
+<script>
+const echarts = require('echarts');
+export default {
+  name: 'echarts',
+  props: ['width','height','option'],
+  mounted() {
+    let chart = echarts.init(this.$refs.echarts);
+    chart.setOption(this.$props.option)
+  },
+  data() {
+    return {
+      // width: '800',
+      // height: '500'
+    }
+  }
+
+}
+</script>
+
