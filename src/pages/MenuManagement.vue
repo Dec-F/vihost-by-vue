78b56e2ab4fa3300298ca07d22c2b847e34755d6
strongfanfan <463428221@qq.com>
2017-05-31 15:47:00 +0800

    tag: tree

--- src/pages/MenuManagement.vue
+++ src/pages/MenuManagement.vue
@@ -20,66 +20,101 @@
 let id = 1000;
 
 export default {
+    
     data() {
         return {
-            menu: [{
-                id: 'root',
-                label: '底栏菜单',
+            menu:
+            [{
+                id: 1,
+                label: '一级 1',
                 children: [{
-                    id: '1',
-                    label: '一级 1',
+                    id: 4,
+                    label: '二级 1-1',
+                    parent: 1,
                     children: [{
-                        id: '1-1',
-                        label: '二级 1-1',
-                        children: [{
-                            id: '1-1-1',
-                            label: '三级 1-1-1'
-                        }, {
-                            id: '1-1-2',
-                            label: '三级 1-1-2'
-                        }]
-                    }]
-                }, {
-                    id: '2',
-                    label: '一级 2',
-                    children: [{
-                        id: '2-1',
-                        label: '二级 2-1'
+                        id: 8,
+                        label: '三级 1-1-1',
+                        parent: 4
                     }, {
-                        id: '2-2',
-                        label: '二级 2-2'
-                    }]
-                }, {
-                    id: '3',
-                    label: '一级 3',
-                    children: [{
-                        id: '3-1',
-                        label: '二级 3-1'
-                    }, {
-                        id: '3-2',
-                        label: '二级 3-2'
+                        id: 9,
+                        label: '三级 1-1-2',
+                        parent: 4
                     }]
                 }]
+            }, {
+                id: 2,
+                label: '一级 2',
+                children: [{
+                    id: 5,
+                    label: '二级 2-1',
+                    parent: 2,
+                }, {
+                    id: 10,
+                    label: '二级 2-2',
+                    parent: 2
+                }]
+            }, {
+                id: 3,
+                label: '一级 3',
+                children: [{
+                    id: 6,
+                    label: '二级 3-1',
+                    parent: 3,
+                }, {
+                    id: 7,
+                    label: '二级 3-2',
+                    parent: 3
+                }]
+
             }]
         }
     },
 
     methods: {
+
+        //通过id在tree上查找节点
+        //参数    root：要解析的根节点    id:触发函数的节点id    isParent：操作父节点？
+        //返回    indexArr:id解析后的数组  node:tree上的数据节点(isParent决定是不是父节点)  currentIndex:触发函数节点的最末级索引
+        resolveData(root, id, isParent = false) {
+            let indexArr = id.split('-'), indexPop = null, node = null;
+            if (isParent) indexPop = parseInt(indexArr.pop());
+            node = indexArr.reduce((acc, val) => {
+                return acc.children[val - 1]
+            }, root)
+
+            return {
+                indexArr: indexArr,
+                node: node,
+                currentIndex: indexPop - 1
+            }
+        },
         insertAfter(store, data) {
-            let indexArr = data.id.split('-'),node=null,
-                indexPop=parseInt(indexArr.pop())
-            console.log(indexPop);
-                node=indexArr.reduce((acc, val) => {
-                    return acc.children[val - 1]
-                }, this.menu[0])
-            node.children.push({id:indexArr.join('-')+'-'+(node.children.length+1),label:"sdsd"})
+            let { indexArr, node, currentIndex } = this.resolveData(this.menu[0], data.id, true);
+
+            let id = indexArr.length === 0 ? (node.children.length + 1).toString() : indexArr.join('-') + '-' + (node.children.length + 1)
+
+            node.children.push({ id: id, label: "新建节点" })
         },
         edit(store, data) {
-            let indexArr = data.id.split('-')
-
+            let { indexArr, node, currentIndex } = this.resolveData(this.menu[0], data.id);
+            console.log(data);
+            console.log(indexArr, node, currentIndex);
+            this.$prompt('菜单名', '编辑', {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消'
+            }, {
+                    confirmButtonText: '确定',
+                    cancelButtonText: '取消'
+                }).then(({ value }) => {
+                    node.label = value
+                })
         },
         remove(store, data) {
-            let indexArr = data.id.split('-')
+            let { indexArr, node, currentIndex } = this.resolveData(this.menu[0], data.id, true);
+
+            console.log(node.children.splice(currentIndex, 1))
+
+            node.ch
         },
 
         renderContent(h, { node, data, store }) {
@@ -90,9 +125,30 @@ export default {
                     </span>
                     <span style="float: right; margin-right: 20px">
                         <i class='el-icon-plus' on-click={() => this.insertAfter(store, data)}></i>
-                        <i class='el-icon-edit' on-click={() => this.remove(store, data)}></i>
+                        <i class='el-icon-edit' on-click={() => this.edit(store, data)}></i>
+                        <i class='el-icon-delete' on-click={() => this.remove(store, data)}></i>
                     </span>
                 </span>);
+        },
+
+        //将tree解析为map，方便编辑操作
+        getMap(arr) {
+            return arr.reduce((acc, val) => {
+
+                acc[val.id] = val
+                
+                if (val.children) {
+                    acc = Object.assign(acc, this.getMap(val.children))
+                }
+
+                return acc
+            }, {})
+        },
+
+    },
+    computed: {
+        map() {
+            return this.getMap(this.menu)
         }
     }
 };
